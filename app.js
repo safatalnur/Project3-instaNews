0a525e3a7ee5c3a807702f69a819243d80d66317
Safat Alnur <55736684+safatalnur@users.noreply.github.com>
2019-10-30 06:16:36 -0400

    Feat: Comments are added

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
 
 jQuery(document).ready(function($){
-
+    
     $("#sectionSelect").on("change", function(event){
-
+    
+    //Change of header style when news are loaded
     $('header').addClass('headSmall');
-        
+    //loader is visible and previous items are removed   
     $('ul').children().remove();
         $("#loader").show();
+        //Adding NYT Api authorization code
         $change = ($(this).val())
        $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$change}.json?api-key=awlzncIf5AWJtGt2d3FyPQ2GxEVYzzFt`)
 
@@ -19,13 +21,12 @@ jQuery(document).ready(function($){
         let totalStories=0;
 // ------
        $.each(data.results, function(key, value) {
-           console.log(value)
-
+            //data for multimedia content
            let multimedia = value.multimedia;
            if (multimedia.length === 0) {
                return
            }
-
+           //collecting and appending abstract, article url, and images
            let description = value.abstract;
            let articleURL = value.url;
 
@@ -41,9 +42,11 @@ jQuery(document).ready(function($){
                 }
 
            })
+                //count and collect number of news upto 12
                 totalStories++;
                 return (totalStories !==12)
     })
+    //Once news are loaded, loader is hidden again
     $('#loader').hide();
 
 })
